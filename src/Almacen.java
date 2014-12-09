a7f21bdb39f90f0874704d96754c67fa65c2b0ae
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-12-09 09:43:55 +0100

    manzana egiten 12

--- src/Almacen.java
+++ src/Almacen.java
@@ -15,11 +15,8 @@ public class Almacen {
 			String [] parte2 = null;
 			String [] parte3 = null;
 			while ((linea != null) || (linea == "")) {
-				//linea = br.readLine();
-				//System.out.println("kk");
 				parte = linea.split(";");
 				Distribuidor distribuidor = new Distribuidor();
-				//System.out.println(linea);
 				parte1 = parte[0].split(",");
 				distribuidor.setNombre(parte1[0]);
 				distribuidor.setCif(parte1[1]);
@@ -40,17 +37,80 @@ public class Almacen {
 				contacto.setTelefono(Integer.parseInt(parte3[3]));
 				distribuidor.setPersonaContacto(contacto);
 
-
-
-				//System.out.println(distribuidor.getNombre());
-				//System.out.println(distribuidor.getCif());
-
-
 				distribuidores.add(distribuidor);
 				linea = br.readLine();
 			}
 
-			Iterator<Distribuidor> itrDistribuidor = distribuidores.iterator();
+			ArrayList<Manzana> amanzana = new ArrayList<Manzana>();
+			Scanner scm = new Scanner(System.in);
+			String nombred;
+			for (int m = 0; m < 2; m++) {
+				Manzana manzana = new Manzana();
+				System.out.println("Tipo manzana");
+				manzana.setTipoManzana(scm.next());
+				System.out.println();
+				manzana.setProcedencia(scm.next());
+				System.out.println();
+				manzana.setColor(scm.next());
+				System.out.println();
+				manzana.setEurosKilo(scm.nextDouble());
+				System.out.println();
+				nombred = scm.next();
+				for (int i = 0; i < distribuidores.size(); i++) {
+					if (nombred.equalsIgnoreCase(distribuidores.get(i).getNombre())) {
+						manzana.setDistribuidor(distribuidores.get(i));
+					}
+				}
+				amanzana.add(manzana);
+			}
+			Iterator<Manzana> itrManzana = amanzana.iterator();
+			while(itrManzana.hasNext()){
+				Manzana manza = itrManzana.next();
+				
+				System.out.println(": " + manza.getTipoManzana());
+				System.out.println("" + manza.getProcedencia());
+				System.out.println("" + manza.getColor());
+				System.out.println("" + manza.getEurosKilo());
+				System.out.println("********Distribuidor********");
+				System.out.println("--Datos--");
+				System.out.println("Nombre: " + manza.getDistribuidor().getNombre());
+				System.out.println("CIF: " + manza.getDistribuidor().getCif());
+				System.out.println("--Direccion--");
+				System.out.println("Población: " + manza.getDistribuidor().getDireccion().getPoblacion());
+				System.out.println("Localidad: " + manza.getDistribuidor().getDireccion().getLocalidad());
+				System.out.println("Calle: " + manza.getDistribuidor().getDireccion().getCalle());
+				System.out.println("Número: " + manza.getDistribuidor().getDireccion().getNumero());
+				System.out.println("--Contacto--");
+				System.out.println("Nombre: " + manza.getDistribuidor().getPersonaContacto().getNombre());
+				System.out.println("Apellido: " + manza.getDistribuidor().getPersonaContacto().getApellido());
+				System.out.println("E-mail: " + manza.getDistribuidor().getPersonaContacto().getEmail());
+				System.out.println("Teléfono: " + manza.getDistribuidor().getPersonaContacto().getTelefono());
+				System.out.println();
+				
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			/*Iterator<Distribuidor> itrDistribuidor = distribuidores.iterator();
 			while(itrDistribuidor.hasNext()){
 				Distribuidor distri = itrDistribuidor.next();
 				
@@ -70,8 +130,7 @@ public class Almacen {
 				System.out.println("Teléfono: " + distri.getPersonaContacto().getTelefono());
 				System.out.println();
 				
-			}
-			//System.out.println("aaa");
+			}*/
 
 			/*for (int i = 0; i < distribuidores.size(); i++) {
 				System.out.println(distribuidores.get(i).getNombre());
